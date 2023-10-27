b4ea26e0e643990c9f284c6ac79c7d636970d07f
gustonecrush <codeboxezz@gmail.com>
2023-10-27 15:40:29 +0700

    edit footer desktop

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,16 +4,16 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer className='bg-primary shadow-soft w-full py-10 px-5 md:px-0'>
-        <div className="flex flex-col gap-6">
-            <div className="flex flex-col gap-2">
+    <footer className='bg-primary shadow-soft w-full py-10 md:py-20 px-5 md:px-20'>
+        <div className="flex flex-col md:flex-row gap-6">
+            <div className="flex flex-col gap-2 w-[45%]">
                 <Link href={'/'} about='Metalcare Homepage Link' title='Metalcare Homepage Link' className='text-4xl text-white font-semibold'>Metal<span className='text-black'>Care</span></Link>
                 <p className='text-sm text-white'>
                 Empower student health and well-being with doctors, psychologists, and peer counselors. We offer tailored medical services and emotional support to address the unique challenges students face.
                 </p>
             </div>
 
-            <div className="flex flex-col gap-2">
+            <div className="flex flex-col gap-2 w-[25%]">
                 <h1 className='text-base font-semibold text-white'>Overview</h1>
                 <div className="flex flex-col gap-1">
                     <p className='text-sm font-regular text-white'>Checking Health</p>
@@ -22,7 +22,7 @@ const Footer = () => {
                 </div>
             </div>
 
-            <div className="flex flex-col gap-2">
+            <div className="flex flex-col gap-2 w-[25%]">
                 <h1 className='text-base font-semibold text-white'>Services</h1>
                 <div className="flex flex-col gap-1">
                     {
@@ -33,7 +33,7 @@ const Footer = () => {
                 </div>
             </div>
 
-            <div className="flex flex-col gap-2">
+            <div className="flex flex-col gap-2 w-[25%]">
                 <h1 className='text-base font-semibold text-white'>Socials</h1>
                 <div className="flex flex-col gap-1">
                     <p className='text-sm font-regular text-white'>Instagram</p>
